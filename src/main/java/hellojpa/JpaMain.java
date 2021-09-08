054711b72f6212e4710c1e5de51c58b310b46115
hj <jun1057@ajou.ac.kr>
2021-09-09 00:57:41 +0900

    프록시의 ==비교대신 instanceof 로 비교해라!

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,35 +13,28 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //팀 저장
             Member member1 = new Member();
             member1.setUsername("member1");
             em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            em.persist(member2);
+//
+//            Member member2 = new Member();
+//            member2.setUsername("member2");
+//            em.persist(member2);
 
             em.flush();
             em.clear();
 
             System.out.println("\n------------------------- FLUSH, CLEAR\n");
             System.out.println("\n------------------------- NEW START\n");
-            Member m1 = em.find(Member.class, member1.getId());
-            Member m2 = em.getReference(Member.class, member2.getId());
-            System.out.println("\nm1 InstanceOf : "+(m1 instanceof Member));
-            System.out.println("\nm2 InstanceOf : "+(m2 instanceof Member));
-            System.out.println("\n m1 == m2 "+(m1 == m2));
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("\nrefmember.getClass() = " + refMember.getClass());//프록시가나와
+            
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("\nfindMember.getClass() = " + findMember.getClass());//객체가나올거라 예상해
 
-//            em.close();
-//            Member refMember = em.getReference(Member.class, member1.getId());
-//            System.out.println("\ngetReference 실행완료");
-//            System.out.println("\n\nrefMember.getUsername() = " + refMember.getUsername());
+            System.out.println("(refMember==findMember) = " + (refMember==findMember));
 
-//            System.out.println(member==refMember);
-//            System.out.println(member==refMember);
             tx.commit();
-
         } catch (Exception e) {
             tx.rollback();
         }finally {
