fd727f16d236a8e52aa44e12df329f5462c25fa9
hj <jun1057@ajou.ac.kr>
2021-09-13 23:08:20 +0900

    Criteria는 작성하기 생각보다 복잡해! 그치만 장점으로는 컴파일 오류를 찾을 수 있어서 동적쿼리를 짜기에 좋긴해!

--- src/main/java/typeStudy/testMain.java
+++ src/main/java/typeStudy/testMain.java
@@ -5,6 +5,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Set;
 
@@ -30,12 +33,23 @@ public class testMain {
 
             em.flush(); em.clear();
             System.out.println("----------------------clear------------");
-            List<Member2> resultList = em.createQuery("select m from Member2 m where m.name like '%KIM%'",Member2.class).getResultList();
+            //Criteria 사용준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member2> query = cb.createQuery(Member2.class);
+            //루트클래스 (조회를 시작할 클래스)
+            Root<Member2> m = query.from(Member2.class);
+
+            //쿼리생성
+            CriteriaQuery<Member2> cq = query.select(m).where(cb.equal(m.get("name"), "KIM"));
+            List<Member2> resultList = em.createQuery(cq).getResultList();
+            System.out.println("\nresultList = " + resultList);
             for (Member2 member2 : resultList) {
-                System.out.println("member2 = " + member2);
+                System.out.println("member2 = " + member2.getName());
             }
+
+
             tx.commit();
-        } catch (Excºeption e) {
+        } catch (Exception e) {
             tx.rollback();
         }finally {
             System.out.println("\n\n---------------------COMMIT------------------");
